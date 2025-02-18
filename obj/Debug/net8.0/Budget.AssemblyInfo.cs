d8cbb47c6a5f564cb530b68c2732d1b362372c1b
jackzhou2 <jack.zhou@nextfortune.tw>
2025-02-18 18:27:26 +0800

    commit

--- obj/Debug/net8.0/Budget.AssemblyInfo.cs
+++ obj/Debug/net8.0/Budget.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Budget")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4244cb2769d575c7acd8eddb442eb0be0f1fea0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Budget")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Budget")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
